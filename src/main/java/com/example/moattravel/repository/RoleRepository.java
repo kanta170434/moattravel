c18f56ae1e8df5c16729de902fc5acc793d09f93
kanta170434 <kanta170434@gmail.com>
2025-03-06 12:22:11 +0900

    3月6日コミット

--- src/main/java/com/example/moattravel/repository/RoleRepository.java
+++ src/main/java/com/example/moattravel/repository/RoleRepository.java
@@ -4,4 +4,5 @@ import com.example.moattravel.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+    public Role findByName(String name);
 }
