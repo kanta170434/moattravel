897f2f6cf0e080e783168fb9f552f4b07a552368
kanta170434 <kanta170434@gmail.com>
2025-03-08 18:01:55 +0900

    3月8日コミット

--- src/main/java/com/example/moattravel/repository/UserRepository.java
+++ src/main/java/com/example/moattravel/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.example.moattravel.repository;
 
 import com.example.moattravel.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     public User findByEmail(String email);
+    public Page<User> findByNameLikeOrFuriganaLike(String nameKeyword, String furiganaKeyword, Pageable pageable);
 }
